ae20ac80e125f879378918ec83f1a9951c20fd37
MichaelOppong <michael@scripted.org>
2020-04-29 18:18:03 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$(".happyBox").hover(function() {
+    $("img").attr("src", "https://media0.giphy.com/media/XbxZ41fWLeRECPsGIJ/giphy.gif?cid=ecf05e474dd6d00c0c685d85b8bfe1623725191da6fbacfd&rid=giphy.gif");
+});
+
+$(".birthdayBox").hover(function() {
+ $("img").attr("src", "https://media1.giphy.com/media/l4KibWpBGWchSqCRy/giphy.gif?cid=ecf05e470d3f7bef0fad21fc70c1cb6fd5b153dcf3de2e18&rid=giphy.gif");
+});
+
+$(".emilyBox").hover(function() {
+   $("img").attr("src", "https://codenation.org/wp-content/uploads/2019/01/IMG_2265.jpg");
+});
